5055cb76fb3fd908d7eac271e428050507e1327c
malenace <62570443+malenace@users.noreply.github.com>
2024-11-20 23:45:38 +0000

    alteração do index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Vivi, eu amo você... Se você me ama, dê uma risadinha</h1>
+    return <h1>Vivi, eu amo você... Se você me ama, dê uma risadinha 🤣</h1>
 }
 
 export default Home;
